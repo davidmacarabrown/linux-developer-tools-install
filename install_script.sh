a44d366793cdc6f05252f93923905eb85d43d7dc
davidmacarabrown <davidmacarabrown@gmail.com>
2021-09-29 22:59:36 +0100

    added PgAdmin Desktop install

--- install_script.sh
+++ install_script.sh
@@ -36,7 +36,6 @@ read -p "Install Mono for C#? [y/n]: " monoanswer
 #TODO: add Postgres under current name
 #TODO: update readme remove sudo instructions, permissions will be increased after script starts
 #TODO: add code . for vscode
-#TODO: POSTICO for postgres GUI
 #TODO: ADD user for postgres
 
 #checking for existing log file and creating one
@@ -209,6 +208,10 @@ wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key a
 apt-get update
 install_package "apt" "postgresql"
 
+#PgAdmin GUI for postgresql
+sudo curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
+sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
+install_package "apt" "pgadmin4-desktop"
 #################################### PLANNING/DESIGN #############################
 
 install_package "snap" "figma"
