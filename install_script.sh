27c4e68a1a9886657f56d0323d85459eb9766a29
davidmacarabrown <davidmacarabrown@gmail.com>
2021-10-03 10:07:36 +0100

    fix: mono install using old function name

--- install_script.sh
+++ install_script.sh
@@ -157,7 +157,7 @@ function install_mono(){
 	apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 	echo "deb https://download.mono-project.com/repo/ubuntu vs-bionic main" | tee /etc/apt/sources.list.d/mono-official-vs.list
 	apt update
-	install_apt "mono-complete"
+	install_package "mono-complete"
 	if [[ $? == 0 ]]
 	then
 		log_result "Mono" "fass"
