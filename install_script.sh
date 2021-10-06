317e1200b5c8d256b0633958880f749daf41f248
David Macara Brown <84098167+davidmacarabrown@users.noreply.github.com>
2021-10-07 00:03:44 +0100

    add: yarn package manager

--- install_script.sh
+++ install_script.sh
@@ -157,6 +157,13 @@ install_package apt npm
 install_package apt nodejs
 install_package apt openjdk-8-jdk
 
+#Yarn
+install_yarn(){
+	curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+	echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+	sudo apt update
+	install_package apt yarn
+}
 ####################################### DATABASE #################################
 
 # MongoDB
